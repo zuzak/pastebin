f038d5f9ef47bbc740ab0e0db4139b9bc2abb447
Douglas Gardner <douglas@chippy.ch>
2014-10-27 14:10:06 +0000

    safer search

--- library.php
+++ library.php
@@ -2,6 +2,12 @@
 function not_stored( $str ) {
 	$str = escapeshellarg( $str );
 	$ret = -1;
-	exec( "grep $str pastes.log", $foo, $ret );
-	return $ret;
+	$contents = file_get_contents( 'pastes.log' );
+	$pattern = preg_quote( $str, '/' );
+	$pattern = "/^.*$pattern.*\$/m";
+	if ( preg_match_all( $pattern, $contents, $matches ) ) {
+		return false;
+	} else {
+		return true;
+	}
 }
