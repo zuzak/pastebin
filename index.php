8dd82a358dcbb75c097b16db38d6fbc01ae79e1a
Douglas Gardner <douglas@chippy.ch>
2014-10-27 15:55:59 +0000

    remove accidental pre-VCS branch

--- index.php
+++ index.php
@@ -5,9 +5,7 @@ require 'library.php';
 if ( isset( $_POST[ 'paste' ] ) ) {
 	require 'paste.php';
 } else if ( $_SERVER[ 'REQUEST_URI' ] !== '/' ) {
-	if ( $_SERVER[ 'REQUEST_URI' ] == '/beta' ) {
-		require 'beta.php';
-	} else if ( substr( $_SERVER[ 'REQUEST_URI' ], 0, 4 ) == '/url' ) {
+	if ( substr( $_SERVER[ 'REQUEST_URI' ], 0, 4 ) == '/url' ) {
 		require 'url.php';
 	} else {
 		require 'render.php';
